17062bcd67b2ec741e1a4d6d20400265637088f0
Sara Mazal <32996161+saramazal@users.noreply.github.com>
2021-12-26 13:34:27 +0200

    my commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import Hello from './components/Hello';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Hello name='Sara' lname='World'/>
+       <Hello name='X' lname='X'/>
+       
+  </div>
+  )
+}
+
+export default App;
---
+++ src/components/Hello.js
@@ -0,0 +1,15 @@
+import logo from '../Kali-7.svg';
+
+function Hello(props) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1> Happy Hacking, {props.name} & {props.lname}!</h1>
+          <h4>Coded by <a href="https://github.com/saramazal" target="_blank">Sara Mazal</a> &copy; 2021</h4>
+              </header>
+      </div>
+    );
+  }
+  
+  export default Hello;
